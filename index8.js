b8cc126cfcde80fb1bebd9a945321ce1bbeec19b
satyamrao789 <satyam987987@gmail.com>
2024-03-17 18:36:46 +0530

    template literals (string interpolation)

--- index8.js
+++ index8.js
@@ -4,3 +4,24 @@ console.log(typeof name);
 console.log(name.length);
 console.log(name[3]);
 
+let user1 = "Alekh";
+let user2 = "Rao";
+
+value1 = "user1 is friend of user2";
+console.log(value1);
+
+//Template literals (also known as String Interpolation)
+value2 = `${user1} is friend of $user2`;
+console.log(value2);
+
+value3 = `${user1} is friend of ${user2}`;
+console.log(value3);
+// value3 =${user1} is friend of ${user2}; uses a template literal to create a new string. Template literals are enclosed in backticks (`) and allow you to embed expressions (variables) directly within the string.
+// %{user1}: This part inserts the value of the variable user1 (which is "Alekh") into the string.
+// ${user2}: This part inserts the value of the variable user2 (which is "Rao") into the string.
+
+
+// Template Literals vs. Concatenation:
+
+// 1. Template literals offer a cleaner and more readable way to construct strings compared to traditional string concatenation using the plus sign (+). 
+// 2. They also prevent accidental typos or spaces when inserting variables.
