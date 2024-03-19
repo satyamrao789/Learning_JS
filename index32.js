4ae6f488ea97270bb59772a537ff04d0a7ee510c
satyamrao789 <satyam987987@gmail.com>
2024-03-20 00:10:49 +0530

    fetching openWeatherMap api

--- index32.js
+++ index32.js
@@ -13,3 +13,13 @@ fetch('https://jsonplaceholder.typicode.com/posts').then((response) =>{
 }).then(data => {
     console.log(data);
 })
+
+//Fetching OP
+
+const API_KEY = '6b75d2d4e2fb1c4ac20ae682594be567';
+
+fetch('https://api.openweathermap.org/data/2.5/weather?lat=24.487520&lon=86.718826&appid='+ API_KEY).then((response) => {
+    return response.json();
+}).then(data => {
+    console.log(data);
+})
